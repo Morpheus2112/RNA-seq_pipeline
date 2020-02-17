bdb497f024c01d6754910efd887f59c2b2f39170
Memphis <palandswd@gmail.com>
2020-02-17 08:43:49 +0100

    the first steps

--- 1-fastqc.sh
+++ 1-fastqc.sh
@@ -8,5 +8,7 @@ module load gcc/4.8.2 gdc java/1.8.0_73 fastqc/0.11.4
 
 DATA=/cluster/project/gdc/people/zhangjic/project_brassica/raw_data/NovaSeq_20191104_NOV226_o6098
 
-
 ls $DATA/*gz | while read id ; do fastqc $id;done 
+
+mv $DATA/*zip .
+mv $DATA/*html .
